254b9c9630daf6c6d5714fceb183991842068431
cesarsandovalsoto <cesandovalsoto@gmail.com>
2022-12-01 15:52:11 -0400

    avances dia 1

--- first.rb
+++ first.rb
@@ -1,1 +1,18 @@
+string = 'my first string'
+puts string.length
 
+puts string.count('i')
+
+puts string.slice(3, 5)
+
+puts string.upcase
+
+puts string.upcase! #todo objeto peligroso mutado con !
+
+puts string.empty?
+
+puts string.include? 'MY'
+
+puts 1000.class
+
+puts 0.1123.class
