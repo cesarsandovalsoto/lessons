52eca0452b7d89b96d04183b29108938cb985f51
cesarsandovalsoto <cesandovalsoto@gmail.com>
2022-12-01 23:51:27 -0300

    cambios desde ghost

--- first.rb
+++ first.rb
@@ -1,19 +1,11 @@
 string = 'my first string'
 puts string.length
-
 puts string.count('i')
-
 puts string.slice(3, 5)
-
 puts string.upcase
-
 puts string.upcase! #todo dangerous object mutated with !
-
 puts string.empty?
-
 puts string.include? 'MY'
-
 puts 1000.class
-
 puts 0.1123.class
 
