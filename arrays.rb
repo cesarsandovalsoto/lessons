5a3badd53c4a1b47b957bf31c00a85ac2dabf88f
cesarsandovalsoto <cesandovalsoto@gmail.com>
2022-12-02 10:43:51 -0300

    update + array

---
+++ arrays.rb
@@ -0,0 +1,7 @@
+#arrays
+#array are order lists stars white 0
+array = [1, "yey", 3.0] #like python
+array = %w{a,b,c,f,g} #todo if all elements in array are strings this is the metod
+
+array = Array.new(3) #rybt metodos starts whit capital letters Array
+array = Array.new(3, 'hola')
