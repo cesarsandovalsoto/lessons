21bff88e3666d4263838a791715acc9b75b1ce40
cesarsandovalsoto <cesandovalsoto@gmail.com>
2022-12-02 11:19:44 -0300

    update + array

--- arrays.rb
+++ arrays.rb
@@ -1,7 +1,10 @@
 #arrays
 #array are order lists stars white 0
-array = [1, "yey", 3.0] #like python
-array = %w{a,b,c,f,g} #todo if all elements in array are strings this is the metod
+#array = [1, "yey", 3.0] #like python
+array = %w{a b c f g} #todo if all elements in array are strings this is the metod there are no , in between
 
-array = Array.new(3) #rybt metodos starts whit capital letters Array
-array = Array.new(3, 'hola')
+#array = Array.new(3) #rybt metodos starts whit capital letters Array
+#array = Array.new(3, 'hola')
+
+puts array[2]
+print array[0..2] #print don't have /l
