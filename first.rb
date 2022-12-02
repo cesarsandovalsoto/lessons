7f53e1462de985ca2e3a7c6567f93bd5a96145d7
cesarsandovalsoto <cesandovalsoto@gmail.com>
2022-12-02 09:23:34 -0300

    #update class time

--- first.rb
+++ first.rb
@@ -8,4 +8,8 @@ puts string.empty?
 puts string.include? 'MY'
 puts 1000.class
 puts 0.1123.class
+puts 1.01.class
 
+puts '4000'.to_i.class
+puts '3.14'.to_f.class
+puts 1994.to_s.class
--- second.rb
+++ second.rb
@@ -1,1 +1,15 @@
-puts 'second'
+#time and date
+
+time = Time.now
+
+puts time.year
+puts time.month
+puts time.day
+
+puts time.hour
+
+puts time.min
+puts time.sec
+puts time.wday
+puts time.yday
+puts time.zone
