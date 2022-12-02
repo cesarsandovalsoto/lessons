1f5cc4afc837186515dec778290be1f4e35d66d1
cesarsandovalsoto <cesandovalsoto@gmail.com>
2022-12-02 11:45:36 -0300

    update + array + rubocop

--- arrays.rb
+++ arrays.rb
@@ -3,8 +3,10 @@
 # array = [1, "yey", 3.0]  like python
 array = %w[a b c f g] # TODO: if all elements in array are strings this is the metod there are no , in between
 
-# array = Array.new(3) #rybt metodos starts whit capital letters Array
-# array = Array.new(3, 'hola')
+# array = Array.new(3) #rybt methods starts whit capital letters Array
+# array = Array.new(3, 'hi')
 
 puts array[2]
 print array[0..2] # print don't have /l
+print array[1, 2]
+
